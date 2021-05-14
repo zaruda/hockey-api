06727fc822d1ebc60d2cf9ab4a2f7bfa129dc58b
Ilya Zarudny <zaruda46@gmail.com>
2021-05-14 17:47:01 +0300

    Add create route

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Inject } from '@nestjs/common';
+import { Controller, Get, Post, Inject } from '@nestjs/common';
 import { ClientProxy } from '@nestjs/microservices';
 import { Observable } from 'rxjs';
 
@@ -6,8 +6,13 @@ import { Observable } from 'rxjs';
 export class AppController {
   constructor(@Inject('GOAL_SERVICE') private client: ClientProxy) {}
 
-  @Get()
+  @Get('/')
+  getAllGoals(): Observable<string> {
+    return this.client.send<string>('get-all', {});
+  }
+
+  @Post('create')
   getHello(): Observable<string> {
-    return this.client.send<string>('hi', 'hello world');
+    return this.client.send<string>('create', { date: Date.now() });
   }
 }
