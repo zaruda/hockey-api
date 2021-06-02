da2f58d063adf8a7c2675f0cc07f69a236c69eb8
Ilya Zarudny <zaruda46@gmail.com>
2021-06-02 17:13:05 +0300

    Implement REST routes

--- src/modules/goal/goal.controller.ts
+++ src/modules/goal/goal.controller.ts
@@ -1,19 +1,54 @@
-import { Controller, Get, Post, Inject } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Inject,
+  Param,
+  Body,
+  Patch,
+  Delete,
+} from '@nestjs/common';
 import { ClientProxy } from '@nestjs/microservices';
 import { Observable } from 'rxjs';
 import { GOAL_SERVICE } from './goal.constants';
 
-@Controller()
-export class GoalController {
-  constructor(@Inject(GOAL_SERVICE) private client: ClientProxy) {}
+type GoalEntity = {
+  date: string;
+};
 
-  @Get('/')
-  getAllGoals(): Observable<string> {
-    return this.client.send<string>('get-all', {});
+type CreateGoalEntity = GoalEntity;
+
+type UpdateGoalEntity = Partial<GoalEntity>;
+
+@Controller('goals')
+export class GoalController {
+  constructor(@Inject(GOAL_SERVICE) private service: ClientProxy) {}
+
+  @Get()
+  async getAllGoals(): Promise<GoalEntity[]> {
+    return await this.service.send('get', {}).toPromise();
   }
 
-  @Post('create')
-  create(): Observable<string> {
-    return this.client.send<string>('create', { date: Date.now() });
+  @Post()
+  create(@Body() goal: CreateGoalEntity): Observable<CreateGoalEntity> {
+    return this.service.send('create', goal);
+  }
+
+  @Get(':id')
+  async getGoalById(@Param('id') id: string): Promise<GoalEntity> {
+    return await this.service.send('getById', id).toPromise();
+  }
+
+  @Patch(':id')
+  updateGoalById(
+    @Param('id') id: string,
+    @Body() goal: Partial<UpdateGoalEntity>,
+  ): Observable<GoalEntity> {
+    return this.service.send('updateById', { id, goal });
+  }
+
+  @Delete(':id')
+  deleteGoalById(@Param('id') id: string): Observable<void> {
+    return this.service.send('deleteById', id);
   }
 }
